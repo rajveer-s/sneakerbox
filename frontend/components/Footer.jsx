6e8af74fb175e5a5f68ac8aecf5dc04a94724657
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 19:23:28 -0700

    menu made for footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -10,8 +10,23 @@ const Footer = () => {
         <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
           {/* Menu  */}
           <div className="flex flex-col gap-3 shrink-0">
-            <div className="font-oswald font-medium uppercase text-sm curosr-pointer">Find a Store</div>
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Find a Store
+            </div>
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Become a Partner
+            </div>
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Sign up for Newsletter
+            </div>
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Send us Feedback
+            </div>
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Student Discount
+            </div>
           </div>
+          {/* Menu End*/}
         </div>
       </Wrapper>
     </footer>

b38adff38f53a58db34e68b7d6c1ff5f3c5c0602
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 23:05:24 -0700

    use client added to navbar and carasouel

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,44 +1,36 @@
-"use client";
-
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/wrapper";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <HeroBanner />
-        <Wrapper>
-          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
-            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-              Cushioning for Your Miles
-            </div>
-            <div className="text-md md:text-xl">
-              A lightweight Nike ZoomX mid-sole is combined with increased stack
-              heights to help provide Cushioning during extended stretches of
-              running.
-            </div>
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
           </div>
-          {/* product cards */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX mid-sole is combined with increased stack
+            heights to help provide Cushioning during extended stretches of
+            running.
           </div>
-          {/* product cards */}
-        </Wrapper>
-      </main>
-      <Footer />
-    </>
+        </div>
+        {/* product cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+        {/* product cards */}
+      </Wrapper>
+    </main>
   );
 }

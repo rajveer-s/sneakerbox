9fcadb641b2d9ed25931cf604cb366fa27a67d8c
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 21:32:36 -0700

    header carousel finsihed

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -5,11 +5,34 @@ import { Carousel } from "react-responsive-carousel";
 
 import { BiArrowBack } from "react-icons/bi";
 import Image from "next/image";
+import { PayButtonElement } from "@stripe/react-stripe-js";
 
 const HeroBanner = () => {
   return (
     <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
-      <Carousel>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showIndicators={false}
+        showArrows={false}
+        renderArrowPrev={(clickHandler, hasPrev) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="text-sm md:text-lg" />
+          </div>
+        )}
+        renderArrowNext={(clickHandler, hasNext) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="rotate-180 text-sm md:text-lg" />
+          </div>
+        )}
+      >
         <div>
           <Image
             src="/slide-1.png"

908511b4527eb1e4b7048026e9b210d0173232d0
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-02 01:58:52 -0700

    payment failed page made

--- frontend/app/cart/page.js
+++ frontend/app/cart/page.js
@@ -60,6 +60,8 @@ const cart = () => {
         </div>
         {/* cart content ends*/}
 
+        {/* empty screen */}
+
         <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
           <Image
             src="/empty-cart.jpg"

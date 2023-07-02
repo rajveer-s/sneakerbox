edf316d52600883a289d79944ee043f766a3b179
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 19:20:34 -0700

    footer icons imported

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import Wrapper from "./wrapper";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-black text-white pt-14 pb-3">
+      <Wrapper className="flex justify-between flex-col md:flex-row gap-[50px md:gap-0]">
+        {/* Left Side of the footer  */}
+        <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
+          {/* Menu  */}
+          <div className="flex flex-col gap-3 shrink-0">
+            <div className="font-oswald font-medium uppercase text-sm curosr-pointer">Find a Store</div>
+          </div>
+        </div>
+      </Wrapper>
+    </footer>
+  );
 };
 
 export default Footer;

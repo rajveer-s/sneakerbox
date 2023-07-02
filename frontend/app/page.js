06bca34988ed133edf2e418bb41fa18fce9571af
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 21:49:00 -0700

    header para

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,6 +3,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/wrapper";
 
 export default function Home() {
   return (
@@ -10,6 +11,18 @@ export default function Home() {
       <Header />
       <main>
         <HeroBanner />
+        <Wrapper>
+          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+              Cushioning for Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              A lightweight Nike ZoomX midsole is combined with increased stak
+              heights to help provide Cushioning during extended stretches of
+              running.
+            </div>
+          </div>
+        </Wrapper>
       </main>
       <Footer />
     </>

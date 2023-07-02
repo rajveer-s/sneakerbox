86ec57bcb85c7bc69d80045e0fb8356eb9d6b7f6
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 19:50:14 -0700

    footer right side made and links added to icons

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 import Wrapper from "./wrapper";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -78,7 +79,56 @@ const Footer = () => {
         {/* Left Side of the footer Ends*/}
 
         {/* Right Side of the footer*/}
+        <div className="flex gap-4 justify-center md:justify-start">
+          <Link href="https://www.facebook.com/" target="_blank">
+            <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+              <FaFacebookF size={20} />
+            </div>
+          </Link>
+          <Link href="https://www.twitter.com/" target="_blank">
+            <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+              <FaTwitter size={20} />
+            </div>
+          </Link>
+          <Link href="https://www.youtube.com/" target="_blank">
+            <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+              <FaYoutube size={20} />
+            </div>
+          </Link>
+          <Link href="https://www.instagram.com/" target="_blank">
+            <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+              <FaInstagram size={20} />
+            </div>
+          </Link>
+        </div>
         {/* Right Side of the footer Ends*/}
+      </Wrapper>
+      <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
+        {/* left side */}
+        <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
+          2023 SneakerBoxFL, Inc. All Rights Reserved
+        </div>
+        {/* left side end*/}
+
+        {/* right side */}
+        <div className="flex gap-2 md:gap-5 text-center md:text-left flex-wrap justify-center">
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Guides
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Terms of Sale
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Terms of Use
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Guides
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Privacy Policy
+          </div>
+        </div>
+        {/* right side ends*/}
       </Wrapper>
     </footer>
   );

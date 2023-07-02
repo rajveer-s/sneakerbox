d382b4f7fbbab5c2dff489752b316f82e03003a7
Rajveer Singh <rajsidhu2924@gmail.com>
2023-07-01 19:33:30 -0700

    left side of the footer made

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -27,7 +27,58 @@ const Footer = () => {
             </div>
           </div>
           {/* Menu End*/}
+
+          {/* Normal Menu  */}
+          <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+            {/* Menu  */}
+            <div className="flex flex-col gap-3">
+              <div className="font-oswald font-medium uppercase text-sm">
+                Get Help
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Order Status
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Delivery
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Returns
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Payment Options
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Contact Us
+              </div>
+            </div>
+            {/* Menu End*/}
+
+            {/* Menu  */}
+            <div className="flex flex-col gap-3">
+              <div className="font-oswald font-medium uppercase text-sm">
+                About SneakerBoxFL
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                News
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Careers
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Investors
+              </div>
+              <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                Sustainability
+              </div>
+            </div>
+            {/* Menu End*/}
+          </div>
+          {/* Normal Menu  Ends*/}
         </div>
+        {/* Left Side of the footer Ends*/}
+
+        {/* Right Side of the footer*/}
+        {/* Right Side of the footer Ends*/}
       </Wrapper>
     </footer>
   );
